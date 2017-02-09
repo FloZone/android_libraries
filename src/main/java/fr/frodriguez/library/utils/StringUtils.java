3caa8bffc2c801673615eb9f9fa940132951cecd
FloZone <flozone45@hotmail.fr>
2017-02-09 23:24:06 +0100

    clean up

--- src/main/java/fr/frodriguez/library/utils/StringUtils.java
+++ src/main/java/fr/frodriguez/library/utils/StringUtils.java
@@ -1,12 +1,25 @@
 package fr.frodriguez.library.utils;
 
+import android.support.annotation.NonNull;
+
 /**
  * By Florian on 22/01/2017.
  */
 
 public class StringUtils {
 
+    /**
+     * Return true if the string is null or empty
+     */
     public static boolean isEmpty(String string) {
         return string == null || string.isEmpty();
+    }
+
+    /**
+     * Return the string value or an empty value if the string is null
+     */
+    @NonNull
+    public static String getValue(String str) {
+        return str == null ? "" : str;
     }
 }
